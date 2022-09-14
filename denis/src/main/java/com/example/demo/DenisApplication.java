d52f0a392887842522244b9199a25ea6b5636bde
FaSTPkS <113505334+FaSTPkS@users.noreply.github.com>
2022-09-14 13:27:00 +0200

    Add files via upload

--- denis/src/main/java/com/example/demo/DenisApplication.java
+++ denis/src/main/java/com/example/demo/DenisApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 @SpringBootApplication
 public class DenisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DenisApplication.class, args);
+
+		ArrayList<User> Usuario = new ArrayList<>();
+
+		System.out.println("Hola");
+
+		for (User usuarios : Usuario) {
+			System.out.println(usuarios.getName());
+		}
 	}
 
 }
