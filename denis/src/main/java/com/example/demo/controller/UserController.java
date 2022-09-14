7e3d21a535f3d99a51fe7ac625ebde3909284e78
FaSTPkS <113505334+FaSTPkS@users.noreply.github.com>
2022-09-14 10:42:03 +0200

    Add files via upload

--- denis/src/main/java/com/example/demo/controller/UserController.java
+++ denis/src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +34,16 @@ public class UserController  {
 		
 	}
 	
+	//Read an user
+	@GetMapping("/{id}")
+	public ResponseEntity<?> read (@PathVariable(value = "id") Long userId){
+		
+		Optional<User> oUser = userService.findById(userId);
+		
+		if(!oUser.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(oUser);
+	}
 }
